352c3d842d126690d4d1067fd95e4359a8099876
codeinn001 <emmaalege@gmail.com>
2021-10-29 13:30:52 +0100

    update

--- assets/js/form-1.js
+++ assets/js/form-1.js
@@ -1,9 +1,10 @@
-let menuButton = document.querySelector('.heading-icon__menu');
+// let menuButton = document.querySelector('.heading-icon__menu');
 let modal = document.querySelector('.modal');
 let cancelButton = document.querySelector('.button--cancel');
 let form = document.querySelector('.form');
 let fieldset = document.querySelector('.fieldset--data');
-let checkboxes = document.querySelector
+let checkboxes = document.querySelectorAll('input[type=checkbox]')
+console.log(checkboxes);
 
 
 function displayModal(hideClass, showClass, mainBgClass) {
@@ -20,6 +21,11 @@ function hideModal(showClass,hideClass,mainBgClass) {
     modal.classList.add(hideClass)
 }
 
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', ()=> {
+        
+    })
+})
 
-menuButton.addEventListener('click', ()=> { displayModal('hide', 'show','main__add-bg')})
+// menuButton.addEventListener('click', ()=> { displayModal('hide', 'show','main__add-bg')})
 cancelButton.addEventListener('click', ()=> { hideModal('show', 'hide', 'main__add-bg')})

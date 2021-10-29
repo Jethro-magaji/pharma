79c053fd79e6e53fd1d1d69175d17f37ef916fc5
codeinn001 <emmaalege@gmail.com>
2021-10-29 09:11:51 +0100

    update

--- assets/js/form-1.js
+++ assets/js/form-1.js
@@ -1,22 +1,20 @@
-let menuButton = document.querySelector('.heading-icon__menu')
-let modal = document.querySelector('.modal')
-let cancelButton = document.querySelector('.button--cancel')
-// let main = document.querySelector('.main')
-let form = document.querySelector('.form')
-let fieldset = document.querySelector('.fieldset--data')
+let menuButton = document.querySelector('.heading-icon__menu');
+let modal = document.querySelector('.modal');
+let cancelButton = document.querySelector('.button--cancel');
+let form = document.querySelector('.form');
+let fieldset = document.querySelector('.fieldset--data');
+let checkboxes = document.querySelector
 
 
 function displayModal(hideClass, showClass, mainBgClass) {
     modal.classList.remove(hideClass)
     modal.classList.add(showClass)
-    // main.classList.add(mainBgClass)
     form.classList.add(mainBgClass)
     fieldset.classList.add(mainBgClass)
 }
 
 function hideModal(showClass,hideClass,mainBgClass) {
     modal.classList.remove(showClass)
-    // main.classList.remove(mainBgClass)
     form.classList.remove(mainBgClass)
     fieldset.classList.remove(mainBgClass)
     modal.classList.add(hideClass)

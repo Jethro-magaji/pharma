6e041fbaecb63f07fa16899af1bed5b2df8fc695
codeinn001 <emmaalege@gmail.com>
2021-10-30 06:19:38 +0100

    update

---
+++ assets/js/login.js
@@ -0,0 +1,9 @@
+// let registerButton = document.querySelector('.login-form--header');
+// let registerField = document.querySelector('.signup--form__fields')
+// console.log(registerButton);
+
+// function displayRegister() {
+//     registerField.classList.toggle('signup--form__fields-display')
+// }
+
+// registerButton.addEventListener('click', displayRegister)

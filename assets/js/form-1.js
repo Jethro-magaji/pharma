9dac1bf58708ce520dfc6f19d77773596f7090d1
codeinn001 <emmaalege@gmail.com>
2021-11-06 22:20:57 +0100

    update

--- assets/js/form-1.js
+++ assets/js/form-1.js
@@ -1,5 +1,5 @@
 let menuButton = document.querySelector('.fa-bars');
-let modal = document.querySelector('.modal');
+let modal = document.querySelector('.modal-custom');
 let cancelButton = document.querySelector('.button--cancel');
 let overlayContainer = document.querySelector('.overlay-container');
 let fieldset = document.querySelector('.fieldset--data');
@@ -7,43 +7,51 @@ let checkboxes = document.querySelectorAll('input[type=checkbox]')
 let pencilIcons = document.querySelectorAll('.bi-pencil-fill');
 let spanLabels = document.querySelectorAll(".label");
 let radioButtons = document.querySelectorAll("input[type=radio]");
-console.log(radioButtons);
-
-console.log(overlayContainer)
 
 
 function displayModal(hideClass, showClass, mainBgClass) {
     modal.classList.remove(hideClass)
     modal.classList.add(showClass)
     overlayContainer.classList.add(mainBgClass)
-    // fieldset.classList.add(mainBgClass)
 }
 
 function hideModal(showClass,hideClass,mainBgClass) {
     modal.classList.remove(showClass)
     overlayContainer.classList.remove(mainBgClass)
-    // fieldset.classList.remove(mainBgClass)
     modal.classList.add(hideClass)
+}
+
+function createAttribute(textInput) {
+    textInput.setAttribute('class', 'pencil-input')
+}
+
+
+function ReplaceLabelWithInput(parent,textInput,label) {
+    parent.replaceChild(textInput, label)   
+    parent.lastElementChild.classList.remove('hide-mic')
+    parent.lastElementChild.classList.add('show-mic')
+}
+
+function hidePencilIcon(pencilIcon) {
+    pencilIcon.style.display = 'none'
 }
 
 pencilIcons.forEach(pencilIcon => {
     pencilIcon.addEventListener('click', ()=> {
     let textInput = document.createElement('input')
-    textInput.setAttribute('class', 'pencil-input')
+    createAttribute(textInput)
+
     let parent = pencilIcon.parentNode 
-    let label = parent.children[2]
-    textInput.value = label.textContent
-    parent.replaceChild(textInput, label)   
-    console.log(parent.lastElementChild);
-    parent.lastElementChild.classList.remove('hide-mic')
-    parent.lastElementChild.classList.add('show-mic')
-    pencilIcon.style.display = 'none'
-    
+    let label = parent.children[2] 
+
+    ReplaceLabelWithInput(parent,textInput, label)
+
+    hidePencilIcon(pencilIcon)
     })
 })
 
-menuButton.addEventListener('click', ()=> { displayModal('hide', 'show','overlay')})
-cancelButton.addEventListener('click', ()=> { hideModal('show', 'hide', 'overlay')})
+menuButton.addEventListener('click', ()=> { displayModal('hide', 'show','overlays')})
+cancelButton.addEventListener('click', ()=> { hideModal('show', 'hide', 'overlays')})
 
 function deselectRadioButton(document, radioButtons) {
  document.addEventListener('mouseup', (e) => {
